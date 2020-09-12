5631cd43a88b76ce6cceaa70734f6f41a0cd22bd
Mitsuriou <dylanpro.jacquemin@gmail.com>
2020-09-12 15:20:21 +0200

    Test for commit-signing setup. Signed-off-by: Mitsuriou <dylanpro.jacquemin@gmail.com>

--- MFBO Preset Creator/buildnumber.h
+++ MFBO Preset Creator/buildnumber.h
@@ -3,7 +3,7 @@
 #ifndef BUILD_NUMBER_H_ 
 #define BUILD_NUMBER_H_ 
 
-#define BUILDNUMBER 60
-#define BUILDNUMBER_STR "60" 
+#define BUILDNUMBER 61
+#define BUILDNUMBER_STR "61" 
 
 #endif /* BUILD_NUMBER_H_ */
