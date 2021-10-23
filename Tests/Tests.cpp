bc6db7750fa278884849556a045b75148c85bce2
Mitsuriou <dylanpro.jacquemin@gmail.com>
2021-10-23 16:05:38 +0200

    Modified tests project configuration to work for Debug target.

--- Tests/Tests.cpp
+++ Tests/Tests.cpp
@@ -17,6 +17,7 @@ namespace Tests
       Assert::IsTrue(DataLists::GetBodyNameVersion(BodyName::CBBE_3BBB_3BA, -1) == BodyNameVersion::INVALID_VALUE);
       Assert::IsTrue(DataLists::GetBodyNameVersion(BodyName::CBBE_3BBB_3BA, 0) == BodyNameVersion::CBBE_3BBB_3BA_1_50);
       Assert::IsTrue(DataLists::GetBodyNameVersion(BodyName::CBBE_3BBB_3BA, 10) == BodyNameVersion::INVALID_VALUE);
+      Assert::IsTrue(DataLists::GetBodyNameVersion(BodyName::CBBE_3BBB_3BA, 10) == BodyNameVersion::CBBE_3BBB_3BA_1_50);
     }
   };
 }
