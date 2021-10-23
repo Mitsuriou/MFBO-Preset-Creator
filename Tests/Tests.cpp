ec9839ae808e6186b1e0b325fd9b2b5d008fce80
Mitsuriou <dylanpro.jacquemin@gmail.com>
2021-10-23 15:55:23 +0200

    Tests working in Release only

---
+++ Tests/Tests.cpp
@@ -0,0 +1,22 @@
+#include "pch.h"
+
+#include "CppUnitTest.h"
+
+// Files to test
+#include "DataLists.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Tests
+{
+  class Tests : public TestClass<Tests>
+  {
+  public:
+    TEST_METHOD(TestGetBodyNameVersion)
+    {
+      Assert::IsTrue(DataLists::GetBodyNameVersion(BodyName::CBBE_3BBB_3BA, -1) == BodyNameVersion::INVALID_VALUE);
+      Assert::IsTrue(DataLists::GetBodyNameVersion(BodyName::CBBE_3BBB_3BA, 0) == BodyNameVersion::CBBE_3BBB_3BA_1_50);
+      Assert::IsTrue(DataLists::GetBodyNameVersion(BodyName::CBBE_3BBB_3BA, 10) == BodyNameVersion::INVALID_VALUE);
+    }
+  };
+}
